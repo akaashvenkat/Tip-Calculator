7e3aab5454966a947a49fae24478db1dec03f058
Akaash Venkat <akaashvenkat@gmail.com>
2020-07-15 14:51:09 -0700

    init setup

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Akaash Venkat on 7/15/20.
+//  Copyright © 2020 Akaash Venkat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+
+
+}
+
